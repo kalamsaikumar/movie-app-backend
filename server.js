3d42ea8179037ffb09d13c5f0490320ee7e32074
kalamsaikumar <saikumarkalam@gmail.com>
2026-07-24 02:16:04 +0530

    Use uppercase PORT environment variable

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,"views",'index.html'));
 })
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port,()=>{
     console.log(`app is listening on http://localhost:${port}`)
